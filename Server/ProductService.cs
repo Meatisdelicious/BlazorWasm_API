949bfbee4086986e11b7eecf4226e90b93a4b417
Adrien Tirlemont <Adrien.Tirlemont@insight.com>
2022-07-13 11:45:56 +0200

    Add project files.

---
+++ Server/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorWasm_API.Shared;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+
+namespace BlazorWasm_API.Server
+{
+    public class ProductService
+    {
+        private List<Product> _products;
+
+        public ProductService()
+        {
+            _products = new List<Product>();
+        }
+        public List<Product> GetProducts()
+        { 
+            return _products;
+        }
+
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+         
+        public void Delete(string productCode)
+        {
+            _products.RemoveAll(a => a.ProductCode == productCode);
+        }
+    }
+}
---
+++ Shared/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasm_API.Shared
+{
+    [Serializable]
+    internal class Product
+    {
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; } 
+        public int ProductPrice { get; set; }  
+    }
+}
